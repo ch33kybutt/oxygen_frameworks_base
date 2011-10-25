ff477ada31b9d40b05f632e81e5354919a556389
ch33kybutt <ch33kybutt@yahoo.com>
2011-10-25 22:01:53 +0800

    Fix build fail due to CMScreenshot update

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1953,6 +1953,9 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_ALWAYS_BATTERY = "lockscreen_always_battery";
 
+        /** @hide */
+        public static final String SHARE_SCREENSHOT = "share_screenshot";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
