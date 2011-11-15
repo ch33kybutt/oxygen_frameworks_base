6a04818a9baf56c04d686d5d1ed0dab9bfa49138
ch33kybutt <ch33kybutt@yahoo.com>
2011-11-15 15:58:33 +0800

    Fix earlier commit: Clear individual notification by swiping left or right.

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -361,10 +361,6 @@ public class NotificationManagerService extends INotificationManager.Stub
             cancelNotification(pkg, tag, id, 0, Notification.FLAG_FOREGROUND_SERVICE);
         }
 
-        public void onNotificationClear(String pkg, String tag, int id) {
-            cancelNotification(pkg, tag, id, 0, Notification.FLAG_FOREGROUND_SERVICE);
-        }
-
         public void onPanelRevealed() {
             synchronized (mNotificationList) {
                 // sound
